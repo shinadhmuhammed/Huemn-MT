023c61196a5f0b0f3575a9e1dee0b76fc4009579
muhammed shinadh <muhammedshinad2@gmail.com>
2024-10-01 08:31:18 +0530

    Deployment error for module not found fixed

--- squarespace/src/Redux/Store/Store.js
+++ squarespace/src/Redux/Store/Store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import siteReducer from '../Slices/siteSlice';
-import workflowReducer from '../Slices/workflowSlice';
-import webReducer from '../Slices/webSlice';
+
+import siteSlice from '../Slices/siteSlice';
+import workflowSlice from '../Slices/workflowSlice';
+import webSlice from '../Slices/webSlice';
 
 const store = configureStore({
   reducer: {
-    site: siteReducer,
-    pageSlice: workflowReducer,
-    web: webReducer, 
+    site: siteSlice,
+    pageSlice: workflowSlice,
+    web: webSlice, 
   },
 });
 export default store;
